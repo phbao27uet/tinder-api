11e56e349dbda33c0b52bb4a129fde151ceaba45
baopham64uete <baopq@solashi.com>
2025-02-13 16:26:04 +0900

    .

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,14 +1,5 @@
-import { randomFillSync } from 'crypto';
-
 export * from './lowercase-first-letter';
 export * from './constants';
 export * from './hash';
 export * from './generate-slug';
 export * from './date';
-export * from './percent-transit-to-vn';
-
-export function generateNumericCode(length = 15): string {
-  return Array.from(randomFillSync(new Uint32Array(length)))
-    .map((x) => x % 10) // Chuyển đổi số ngẫu nhiên thành chữ số từ 0-9
-    .join('');
-}
--- src/shared/utils/percent-transit-to-vn.ts
+++
@@ -1,7 +0,0 @@
-export const percentTransitToVN = (inTransitVNAt: string) => {
-  const diffTime = new Date().getTime() - new Date(inTransitVNAt).getTime();
-  const percent = Math.floor(diffTime / ((1000 * 60 * 60) / 1.5));
-  const value = percent < 0 ? 0 : percent > 99 ? 99 : percent;
-
-  return value;
-};
